fce72bac36f8a246c00a965c3ad778ff17f4faa5
xuancanhit99 <xuancanhit99@gmail.com>
2020-09-12 12:07:28 +0300

    Update task2

--- src/src/ru/mirea/canh/pr3/task2/TestShape.java
+++ src/src/ru/mirea/canh/pr3/task2/TestShape.java
@@ -5,18 +5,17 @@ public class TestShape {
         Shape s1 = new Circle(5.3, "red", true);
         Shape s2 = new Rectangle(5, 6, "blue", true);
         Shape s3 = new Square(8, "black", false);
+
         s1.setColor("while");
         s1.setFiller(false);
-
         System.out.println(s1);
         System.out.println("- Area: " + s1.getArea());
         System.out.println("- Perimeter: " + s1.getPerimeter());
 
         System.out.println(s2);
         System.out.println("- Area: " + s2.getArea());
-
         System.out.println("- Perimeter: " + s2.getPerimeter());
-        System.out.println(s3);
 
+        System.out.println(s3);
     }
 }
