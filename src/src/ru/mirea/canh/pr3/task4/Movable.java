b5b68c3c874a4c2705af49502faeb79532b8efcc
xuancanhit99 <xuancanhit99@gmail.com>
2020-09-14 03:03:51 +0300

    Final pr3

---
+++ src/src/ru/mirea/canh/pr3/task4/Movable.java
@@ -0,0 +1,8 @@
+package src.ru.mirea.canh.pr3.task4;
+
+public interface Movable {
+    public void moveUp();
+    public void moveDown();
+    public void moveLeft();
+    public void moveRight();
+}
---
+++ src/src/ru/mirea/canh/pr3/task5/TestMovable.java
@@ -0,0 +1,17 @@
+package src.ru.mirea.canh.pr3.task5;
+
+public class TestMovable {
+    public static void main(String[] args) {
+        Movable m1 = new MovableRectangle(5, 10,15,20, 5, 5);
+        System.out.println(m1);
+        m1.moveUp();
+        m1.moveRight();
+        System.out.println(m1);
+
+        Movable m2 = new MovableRectangle(7, 9,14,27, 6, 8);
+        System.out.println(m2);
+        m1.moveDown();
+        m1.moveLeft();
+        System.out.println(m2);
+    }
+}
